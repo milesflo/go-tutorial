42d41633b39ffd649fc6e1541bf9758a47cde464
Miles <miles.b.flo@gmail.com>
2018-10-10 22:19:38 -0700

    I've hit my head against this source directory thing long enough.. Heading over to code wars

---
+++ p7/src/geometry/geometry.go
@@ -0,0 +1,10 @@
+package main
+// This file is the main invocation of the script/app. Package must be called main
+
+import "fmt" // `fmt` stands for format by the way
+	// "rectangle/rectprops"
+
+
+func main() {
+	fmt.Println("Geometric properties")
+}
---
+++ p7/src/geometry/rectangle/rectprops.go
@@ -0,0 +1,14 @@
+package rectangle
+// This is a package, not meant for main execution. Call it anything other than main
+
+import "math"
+
+func Area(len, wid float64) float64 {
+	area := len * wid
+	return area
+}
+
+func Diagonal(len, wid float64) float64 {
+	diagonal := math.Sqrt((len * len) + (wid * wid))
+	return diagonal
+}
